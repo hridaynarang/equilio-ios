6288ea0d02f6eda4868c608295c2739363ab390b
Hriday Narang <hridaynarang@gmail.com>
2025-06-15 14:43:00 -0400

    Initial commit

---
+++ Equilio/EquilioApp.swift
@@ -0,0 +1,17 @@
+//
+//  EquilioApp.swift
+//  Equilio
+//
+//  Created by Hriday Narang on 6/15/25.
+//
+
+import SwiftUI
+
+@main
+struct EquilioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ EquilioTests/EquilioTests.swift
@@ -0,0 +1,16 @@
+//
+//  EquilioTests.swift
+//  EquilioTests
+//
+//  Created by Hriday Narang on 6/15/25.
+//
+
+import Testing
+
+struct EquilioTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
